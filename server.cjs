bad0e03a2ae62f9f36f3b9132121e248510a495c
giorgi khoshtaria <99177635+g1org11@users.noreply.github.com>
2024-08-22 13:12:48 +0400

    add cors

--- server.cjs
+++ server.cjs
@@ -11,15 +11,21 @@ const path = require("path");
 require("dotenv").config();
 // const fonturl = process.env.REACT_APP_FONT_URL;
 // app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-// app.options(
-//   "*",
-//   cors({
-//     origin: fonturl,
-//     credentials: true,
-//     allowedHeaders: "Content-Type,Authorization",
-//   })
-// );
+app.use(
+  cors({
+    origin: fonturl,
+    credentials: true,
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
+app.options(
+  "*",
+  cors({
+    origin: fonturl,
+    credentials: true,
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
 // Increase max header size
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ limit: "1mb", extended: true }));
